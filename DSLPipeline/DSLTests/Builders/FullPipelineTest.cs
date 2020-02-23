dbe0a917db4e43008c94e66f2a7a61d5dd7bbe7b
Christian Clausen <csbc92@hotmail.com>
2020-02-24 00:54:07 +0100

    Added the pipeline from TANKS

--- DSLPipeline/DSLTests/Builders/FullPipelineTest.cs
+++ DSLPipeline/DSLTests/Builders/FullPipelineTest.cs
@@ -47,5 +47,83 @@ namespace DSLTests
             builder.Build();
             Pipeline p = builder.Collect();
         }
+
+        [Test]
+        public void InstantiateTANKSPipelineTest()
+        {
+            IPipelineBuilder builder = new PipelineBuilderImpl();
+
+            string tanksDir = "./TANKS";
+            string checkoutAction = "actions/checkout@v2";
+            string dockerAction = "docker://cfeicommon/maven-jdk8-fx:latest";
+            
+            builder.Pipeline("CI")
+                .TriggerOn(TriggerType.Push)
+                
+                // BUG: The builder crashes if there are no globals added
+                .AddGlobals().
+                RunsOn(OperatingSystem.UbuntuLatest).
+                SetEnvVar("MY_ENV_VAR", "HELLO WORLD!").
+                AddStep("Default Checkout step").
+                AsAction().
+                Execute("actions/checkout@v2")
+                
+                .AddJob("compile")
+                    .RunsOn(OperatingSystem.UbuntuLatest)
+                    .AddStep("Checkout repo")
+                        .AsAction()
+                            .Execute(checkoutAction)
+                    .AddStep("Pull Docker Image")
+                        .AsAction()
+                            .Execute(dockerAction)
+                    .AddStep("Compile Tanks")
+                        .AsShell()
+                            .Execute("mvn compile")
+                            .InDirectory(tanksDir)
+                .AddJob("unit-test")
+                    .RunsOn(OperatingSystem.UbuntuLatest)
+                    .DependsOn("compile")
+                    .AddStep("Checkout repo")
+                        .AsAction()
+                            .Execute(checkoutAction)
+                    .AddStep("Pull Docker Image")
+                        .AsAction()
+                            .Execute(dockerAction)
+                    .AddStep("Execute unit test TANKS")
+                        .AsShell()
+                            .Execute("Running mvn verify")
+                            .Execute("mvn verify")
+                            .InDirectory(tanksDir)
+                .AddJob("package")
+                    .RunsOn(OperatingSystem.UbuntuLatest)
+                    .DependsOn("unit-test")
+                    .AddStep("Checkout repo")
+                        .AsAction()
+                        .Execute(checkoutAction)
+                    .AddStep("Pull Docker Image")
+                        .AsAction()
+                        .Execute(dockerAction)
+                    .AddStep("Package TANKS")
+                        .AsShell()
+                            .Execute("mvn package")
+                            .InDirectory(tanksDir)
+                .AddJob("install")
+                    .RunsOn(OperatingSystem.UbuntuLatest)
+                    .DependsOn("package")
+                    .AddStep("Checkout repo")
+                        .AsAction()
+                            .Execute(checkoutAction)
+                    .AddStep("Pull Docker Image")
+                        .AsAction()
+                            .Execute(dockerAction)
+                    .AddStep("Install TANKS")
+                        .AsShell()
+                            .Execute("mvn install")
+                            .InDirectory(tanksDir);
+            
+            builder.Build();
+            Pipeline p =builder.Collect();
+
+        }
     }
 }
